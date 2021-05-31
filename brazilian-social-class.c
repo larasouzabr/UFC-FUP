7890ba4081d423d2a7e9d72cadd94bdaa55c0e2f
Lara Souza <80922589+larasouzabr@users.noreply.github.com>
2021-05-31 15:23:04 -0300

    Create brazilian-social-class.c

---
+++ brazilian-social-class.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main (){
+   float renda;
+    scanf("%f", &renda);
+    
+if (renda <= 324 ) printf ("Extremamente pobre");
+else if ( renda <= 648) printf ("Pobre");
+else if ( renda <= 1164 ) printf ("Vulneravel");  
+else if ( renda <= 1764) printf ("Baixa classe media");
+else if (renda <= 2564) printf ("Media classe media");
+else if ( renda <= 4076) printf ("Alta classe media");
+else if ( renda <= 9920) printf ("Baixa classe alta");
+else printf ("Alta classe alta");
+    return 1;
+}
