baa71f9b6f3c34f2b738c8596d0f96248f72ddcc
Lara Souza <80922589+larasouzabr@users.noreply.github.com>
2021-06-14 15:44:12 -0300

    Update have-i-been-aproved.c

--- have-i-been-aproved.c
+++ have-i-been-aproved.c
@@ -1,3 +1,11 @@
+/*We will have 3 notes. In the following situations, the student will be approved:
+1 - If the average of the 3 grades is greater than or equal to 7 the student is approved.
+2 - If the average is greater than 5 and your third grade is greater than or equal to 7.
+3 - If the difference between the highest grade and the lowest grade is less than or equal to 3.
+Otherwise the student will already fail.
+the program will receive the student's three grades and print "Approved" if the student passed or "Failed".*/
+
+
 #include <stdio.h>
 int main (){
     float nota1,nota2,nota3;
